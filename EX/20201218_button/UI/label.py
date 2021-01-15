3c961d7de3afcad7692ebea0e77d0cd45948f038
garyhsieh <garyhsieh@garyhsiehs-iMac.AirDream>
2021-01-15 16:41:49 +0800

    add 20201218 button

---
+++ EX/20201218_button/UI/label.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'mainwindow.ui'
+#
+# Created by: PyQt5 UI code generator 5.9.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(200, 260, 113, 32))
+        self.pushButton.setObjectName("pushButton")
+        self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_2.setGeometry(QtCore.QRect(370, 340, 113, 32))
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(60, 40, 60, 16))
+        self.label.setObjectName("label")
+        self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit.setGeometry(QtCore.QRect(170, 40, 113, 21))
+        self.lineEdit.setObjectName("lineEdit")
+        self.checkBox = QtWidgets.QCheckBox(self.centralwidget)
+        self.checkBox.setGeometry(QtCore.QRect(60, 10, 87, 20))
+        self.checkBox.setObjectName("checkBox")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 22))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        self.pushButton.clicked.connect(MainWindow.close)
+        self.pushButton_2.clicked.connect(MainWindow.close)
+        #self.checkBox.clicked['bool'].connect(self.label.setVisible)
+        #self.checkBox.clicked['bool'].connect(self.lineEdit.setEnabled)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.pushButton.setText(_translate("MainWindow", "PushButton"))
+        self.pushButton_2.setText(_translate("MainWindow", "PushButton"))
+        self.label.setText(_translate("MainWindow", "TextLabel"))
+        self.checkBox.setText(_translate("MainWindow", "CheckBox"))
+
---
+++ EX/20201218_button/main.py
@@ -0,0 +1,38 @@
+
+from UI.label import Ui_MainWindow
+
+import sys
+import time
+
+from PyQt5.QtWidgets import QApplication, \
+        QMainWindow, \
+        QWidget
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+        self.ui.label.setVisible(False)
+        self.ui.lineEdit.setDisabled(True)
+
+        self.ui.pushButton_2.clicked.connect(self.close)
+        self.ui.checkBox.clicked['bool'].connect(self.ui.lineEdit.setEnabled)
+        self.ui.checkBox.clicked['bool'].connect(self.ui.label.setVisible)
+
+
+
+if __name__ == '__main__':
+    # 新增一個Qt應用程式，管理所有的Qt物件資源(唯一一個), 並且傳入sys.argv作為初始化資料
+    # sys：用於Qt開始的參數
+    # 每個Qt程式只會有一個 QApplication 作為管理所有Qt資源的管理程式
+    app = QApplication(sys.argv)
+    # 新增一個MainWindow物件(QMainWindow物件)
+    window = MainWindow()
+    # 顯示視窗
+    window.show()
+    # app.exec_() 進入QApplication程式，也就是說開始運行Qt GUI程式,
+    # 當app.exec_()執行完畢時，關閉python。
+    sys.exit(app.exec_()) 
